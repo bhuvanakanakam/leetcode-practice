48581e1b3e9153457f0357a8fd0715be75111648
Bhuvana Kanakam <116140522+bhuvanakanakam@users.noreply.github.com>
2025-01-03 13:14:59 +0530

    rightAngledNumberedPyramid.java

---
+++ question-types/patterns/rightAngledNumberedPyramid.java
@@ -0,0 +1,10 @@
+class Main{
+  static void example3(int N){
+    for (int i=1; i<=N; i++){
+      for (int j=1; j<=i; j++){
+        System.out.print(j+" ");
+      }
+      System.out.println();
+    }
+  }
+}
