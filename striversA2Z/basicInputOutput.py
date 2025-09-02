fdf1b71e48f981c91b7850a954c71142ced2c48f
Bhuvana Kanakam <116140522+bhuvanakanakam@users.noreply.github.com>
2025-09-01 18:49:00 -0700

    Basic Input Output
    basic input and output in python, use of input()

---
+++ striversA2Z/basicInputOutput.py
@@ -0,0 +1,8 @@
+// Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+// Input(user gives value): 7
+// Output: 7
+
+class Solution:
+  def printNumber(self):
+    integerNumber = input();
+    print(integerNumber);
